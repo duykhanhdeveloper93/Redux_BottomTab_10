6a61847e402082848e8eb7c75f4a93462415d798
pduykhanh <pduykhanh@vnpt.vn>
2022-01-10 18:25:58 +0700

    update_in_office

--- appFolder/componentScreen/part4/Header.js
+++ appFolder/componentScreen/part4/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, ImageBackground, Text } from 'react-native'
+import { View, ImageBackground, Text, Image } from 'react-native'
 
 import { useSelector } from 'react-redux'
 
@@ -8,16 +8,41 @@ const Header = () => {
     const reduxData = useSelector(reduxSource => reduxSource).messageReducer;
 
     return (
-        <View style={{ flex: 1}}>
+        <View style={{ flex: 1 }}>
             <View >
                 <ImageBackground
-                    resizeMode="contain"
-                    style={{ height: 200 }}
+                    resizeMode="cover"
+                    style={{ height: 190 }}
                     source={reduxData.bgImage1}>
-                    <View style={{ marginVertical: 20, marginHorizontal: 18}}>
-                        <View style={{flexDirection: 'row'}}>
-                            <Text style={{color: 'white', fontSize: 18, fontWeight: '600'}}>{reduxData.text_1_1}</Text>
+                    <View style={{ marginVertical: 20, marginHorizontal: 18 }}>
+                        <View style={{ flexDirection: 'row' }}>
+
+                            <View style={{ flex: 2 }}>
+                                <Text style={{ color: 'white', fontSize: 18, fontWeight: '600' }}>{reduxData.text_1_1}</Text>
+                            </View>
+                            <View style={{ flex: 3 }}>
+
+                            </View>
+                            <View style={{ justifyContent: 'space-around', flexDirection: 'row', flex: 1 }}>
+                                <Image style={{}} resizeMode="contain" source={reduxData.scanIcon} />
+                                <Image style={{}} resizeMode="contain" source={reduxData.searchIcon} />
+                            </View>
+
                         </View>
+
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        position: 'absolute',
+                        bottom: -50,
+                        left: 20,
+                        backgroundColor: 'white',
+                        height: 102,
+                        width: 339,
+                        borderRadius: 20
+                    }}>
+
+
 
                     </View>
                 </ImageBackground>
--- appFolder/reduxFolder/reducers/MessageReducer.js
+++ appFolder/reduxFolder/reducers/MessageReducer.js
@@ -8,6 +8,8 @@ const initialState= {
     // Line 1
     bgImage1 : require ('../../sourceData/image/part4/header/bgImage1/img.png'),
     text_1_1 : "Message",
+    scanIcon : require ('../../sourceData/image/part4/header/scan/icon.png'),
+    searchIcon : require ('../../sourceData/image/part4/header/search/icon.png'),
 }
 
 function MessageReducer(state = initialState, action) {
